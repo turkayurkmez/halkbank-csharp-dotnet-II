724fab434f95871c75e1183863ad450fbf3aee76
Türkay Ürkmez <turkay@turkayurkmez.com>
2024-02-09 12:34:33 +0300

    Dependency Inversion principle & injection pattern

---
+++ CSharp/SpesificTopics/DependencyInversion/DependencyInversion/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DependencyInversion;
+
+Console.WriteLine("Hello, World!");
+
+MailSender mailSender = new MailSender();
+WhatsAppSender whatsSender = new WhatsAppSender();
+TelegramSender TelegramSender = new TelegramSender();
+Report report = new Report(mailSender);
+Report report2 = new Report(whatsSender);
+Report report3 = new Report(TelegramSender);
+
+
+
---
+++ CSharp/SpesificTopics/DependencyInversion/DependencyInversion/Report.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInversion
+{
+
+    public interface ISender
+    {
+        void Send();
+    }
+    public class Report
+    {
+        private ISender sender;
+        public Report(ISender sender)
+        {
+            this.sender = sender;
+        }
+        //  public MailSender MailSender { get; set; }
+        public void Send()
+        {
+            sender.Send();
+        }
+    }
+
+    public class MailSender : ISender
+    {
+        public void Send() => Console.WriteLine("Eposta gönderildi!");
+    }
+
+    public class WhatsAppSender : ISender
+    {
+        public void Send() => Console.WriteLine("Whatsapp gönderildi!");
+    }
+
+    public class TelegramSender : ISender
+    {
+        public void Send() => Console.WriteLine("Telegram gönderildi!");
+    }
+}
